eb100877ca7cd9e5e95fd2fac3210bf56c00db8a
timothymuchena <chingarandew@gmail.com>
2016-02-22 21:54:59 +0200

    Added files via upload

---
+++ app/components/App.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import Notes from './Notes.jsx';
+
+import NoteActions from '../actions/NoteActions';
+import NoteStore from '../stores/NoteStore';
+
+export default class App extends React.Component {
+  
+	constructor(props){
+		super(props);
+
+		this.state =NoteStore.getState();
+	}
+    componentDidMount() {
+    	NoteStore.listen(this.storeChanged);
+    }
+    componentWillUnmount() {
+    	NoteStore.unlisten(this.storeChanged);
+    }
+    storeChanged = (store) => {
+    	this.setState(state);
+    };
+
+    render() {
+		const notes = this.state.notes;
+
+	    return (
+	      <div>
+
+	        <button className="add-note" onClick={this.addNote}>+</button>
+
+	        <Notes notes={notes}
+	         onEdit={this.editNote}
+	         onDelete={this.deleteNote} />
+	      </div>
+	    );
+	}
+
+	deleteNote (id) {
+		NoteActions.delete(id);
+	}
+
+    addNote () {
+		NoteActions.create({task: 'New task'});
+	}
+
+	editNote (id, task) {
+		if(!task.trim()) {
+			return;
+		}
+
+		NoteActions.update({id, task});
+	}
+}
